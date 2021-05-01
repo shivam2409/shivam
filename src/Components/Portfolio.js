1b520fb0d0ade7bb5ae51279718e3b0912af8762
Shivam Patel <51165698+shivam2409@users.noreply.github.com>
2021-05-01 15:24:17 -0400

    finishing up

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import codegram from '../codegram.png';
-import portfolio from '../portfolio.png';
-import netflix from '../netflix.png';
+import imagenest from '../imagenest.png';
+import flexmart from '../flexmart.png';
 //Font awesome import
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearchPlus } from '@fortawesome/free-solid-svg-icons';
@@ -10,7 +10,7 @@ import { PopupboxManager, PopupboxContainer } from 'react-popupbox';
 import 'react-popupbox/dist/react-popupbox.css';
 
 const Portfolio = () => {
-  const openPopupboxRestromation = () => {
+  const openPopupboxcodegram = () => {
     const content = (
       <>
         <img
@@ -31,28 +31,36 @@ const Portfolio = () => {
         >
           hhttps://github.com/shivam2409/codegram
         </a>
+        <br />
+        <b>Deployed at:</b>
+        <a
+          className='hyper-link'
+          onClick={() => window.open('https://codegram24.herokuapp.com/')}
+        >
+          https://codegram24.herokuapp.com/
+        </a>
       </>
     );
     PopupboxManager.open({ content });
   };
 
-  const popupboxConfigRestromation = {
+  const popupboxConfigcodegram = {
     titleBar: {
       enable: true,
-      text: 'Restaurant automation project.',
+      text: 'Codegram',
     },
     fadeIn: true,
     fadeInSpeed: 500,
   };
 
   //Portfolio project
-  const openPopupboxPortfolio = () => {
+  const openPopupboxImagenest = () => {
     const content = (
       <>
         <img
           className='portfolio-image-popupbox'
-          src={portfolio}
-          alt='Portfolio project ... '
+          src={imagenest}
+          alt='Imagenest project ... '
         />
         <p>
           The current project developed using React. Find out more at my github
@@ -61,66 +69,62 @@ const Portfolio = () => {
         <b>Github:</b>
         <a
           className='hyper-link'
-          onClick={() =>
-            window.open('https://github.com/himanshushah96/portfolio')
-          }
+          onClick={() => window.open('https://github.com/shivam2409/imagenest')}
         >
-          https://github.com/himanshushah96/portfolio
+          https://github.com/shivam2409/imagenest
         </a>
       </>
     );
     PopupboxManager.open({ content });
   };
 
-  const popupboxConfigPortfolio = {
+  const popupboxConfigImagenest = {
     titleBar: {
       enable: true,
-      text: 'Portfolio project.',
+      text: 'Imagenest project.',
     },
     fadeIn: true,
     fadeInSpeed: 500,
   };
 
-  //Netflix clone
+  //flexmart
 
-  const openPopupboxNetflix = () => {
+  const openPopupboxflexmart = () => {
     const content = (
       <>
         <img
           className='portfolio-image-popupbox'
-          src={netflix}
-          alt='Netflix clone project ... '
+          src={flexmart}
+          alt='flexmart clone project ... '
         />
         <p className='portfolio-para'>
-          Tried to build netflix web app screen using reactJs Created using the
+          Tried to build flexmart web app screen using reactJs Created using the
           api from tmdb. Deployed on Firebase
         </p>
         <b>Github:</b>
         <a
           className='hyper-link'
-          onClick={() =>
-            window.open('https://github.com/himanshushah96/Netflix-clone')
-          }
+          onClick={() => window.open('https://github.com/shivam2409/flexmart')}
         >
-          https://github.com/himanshushah96/Netflix-clone
+          https://github.com/shivam2409/flexmart
         </a>
         <br />
         <b>Deployed at:</b>
         <a
           className='hyper-link'
-          onClick={() => window.open('https://netflix-clone-3f5a0.web.app/')}
+          onClick={() => window.open('https://github.com/shivam2409/flexmart')}
         >
-          https://netflix-clone-3f5a0.web.app/
+          https://github.com/shivam2409/flexmart
         </a>
       </>
     );
     PopupboxManager.open({ content });
   };
 
-  const popupboxConfigNetflix = {
+  const popupboxConfigflexmart = {
     titleBar: {
       enable: true,
-      text: 'Netflix clone project.',
+      text: '',
     },
     fadeIn: true,
     fadeInSpeed: 500,
@@ -131,37 +135,26 @@ const Portfolio = () => {
       <div className='container'>
         <h1 className='text-uppercase text-center py-5'>Projects</h1>
         <div className='image-box-wrapper row justify-content-center'>
-          <div
-            className='portfolio-image-box'
-            onClick={openPopupboxRestromation}
-          >
-            <img
-              className='portfolio-image'
-              src={codegram}
-              alt='Restaurant Automation...'
-            />
+          <div className='portfolio-image-box' onClick={openPopupboxcodegram}>
+            <img className='portfolio-image' src={codegram} alt='Codegram' />
             <div className='overflow'></div>
             <FontAwesomeIcon className='portfolio-icon' icon={faSearchPlus} />
           </div>
 
           {/*--*/}
 
-          <div className='portfolio-image-box' onClick={openPopupboxPortfolio}>
+          <div className='portfolio-image-box' onClick={openPopupboxImagenest}>
             <img
               className='portfolio-image'
-              src={portfolio}
+              src={imagenest}
               alt='Portfolio...'
             />
             <div className='overflow'></div>
             <FontAwesomeIcon className='portfolio-icon' icon={faSearchPlus} />
           </div>
 
-          <div className='portfolio-image-box' onClick={openPopupboxNetflix}>
-            <img
-              className='portfolio-image'
-              src={netflix}
-              alt='Netflix Clone...'
-            />
+          <div className='portfolio-image-box' onClick={openPopupboxflexmart}>
+            <img className='portfolio-image' src={flexmart} alt='Flexmart' />
             <div className='overflow'></div>
             <FontAwesomeIcon className='portfolio-icon' icon={faSearchPlus} />
           </div>
@@ -171,9 +164,9 @@ const Portfolio = () => {
         </p>
       </div>
 
-      <PopupboxContainer {...popupboxConfigRestromation} />
-      <PopupboxContainer {...popupboxConfigPortfolio} />
-      <PopupboxContainer {...popupboxConfigNetflix} />
+      <PopupboxContainer {...popupboxConfigcodegram} />
+      <PopupboxContainer {...popupboxConfigImagenest} />
+      <PopupboxContainer {...popupboxConfigflexmart} />
     </div>
   );
 };
